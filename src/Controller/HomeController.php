597e93339a3efd30ef8d36e014359a4c08640823
damienboiteux.com <contact@damienboiteux.com>
2023-02-08 12:26:49 +0100

    TWIG initiation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,44 @@
 
 namespace App\Controller;
 
+use stdClass;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/', name: "home")]
     public function index(): Response
     {
-        return new Response('<body><h1>Page d\'accueil</h1></body>');
+        $tableau = ['a', 'b', 'c'];
+
+        $objet = new stdClass();
+
+        $compte = new StdClass();
+        $compte->login = 'tutu';
+        $compte->password = 'tutu';
+
+        $liste = [
+            ['login' => 'toto', 'password' => 'azerty'],
+            ['password' => 'qsdfghh', 'login' => 'titi'],
+            ['login' => 'tata', 'password' => 'admin'],
+            $compte,
+        ];
+
+        $objet->nom = "Dupont";
+        $objet->prenom = "Paul";
+        dump($objet);
+
+        return $this->render(
+            'home/homepage.html.twig',
+            [
+                'titre'     =>  'Je suis la page d\'accueil',
+                'tableau'   =>  $tableau,
+                'personne'  =>  $objet,
+                'personnes' =>  $liste,
+            ]
+        );
     }
 }
