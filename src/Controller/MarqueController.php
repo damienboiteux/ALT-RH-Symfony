0c2c09911079c7441d38aa454c8e708ac4ca528b
damienboiteux.com <contact@damienboiteux.com>
2023-02-09 10:19:09 +0100

    Ajout marque/show

--- src/Controller/MarqueController.php
+++ src/Controller/MarqueController.php
@@ -9,34 +9,47 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/marques')]
 class MarqueController extends AbstractController
 {
+    private array $marques  = [
+        ['id' => 1, 'nom' => 'Fiat', 'slug' => 'fiat'],
+        ['id' => 2, 'nom' => 'Ford', 'slug' => 'ford'],
+        ['id' => 3, 'nom' => 'BMW', 'slug' => 'bmw'],
+        ['id' => 4, 'nom' => 'Tesla', 'slug' => 'tesla'],
+        ['id' => 5, 'nom' => 'Audi', 'slug' => 'audi'],
+    ];
 
     #[Route('/', name: "marque_liste")]
     public function index(): Response
     {
-
-        $marques = [
-            ['id' => 1, 'nom' => 'Fiat', 'slug' => 'fiat'],
-            ['id' => 2, 'nom' => 'Ford', 'slug' => 'ford'],
-            ['id' => 3, 'nom' => 'BMW', 'slug' => 'bmw'],
-            ['id' => 4, 'nom' => 'Tesla', 'slug' => 'tesla'],
-            ['id' => 5, 'nom' => 'Audi', 'slug' => 'audi'],
-        ];
-
         return $this->render('marque/liste.html.twig', [
-            'marques' => $marques,
+            'marques' => $this->marques,
         ]);
     }
 
     #[Route('/new', name: "marque_new")]
     public function new(): Response
     {
-        return new Response('Nouvelle marque');
+
+        return $this->render('marque/new.html.twig');
     }
 
     #[Route('/{slug}', requirements: ['slug' => '[a-z-]+'], name: "marque_show")]
     public function show(string $slug): Response
     {
-        return new Response('Détails marque ' . $slug);
+
+        // entrée : slug
+        $marque = [];
+        foreach ($this->marques as $value) {
+            // Pour chaque ligne du tableau
+            if ($value['slug'] === $slug) {
+                $marque = $value;
+                break;
+            }
+        }
+
+        // -> vue : ligne qui correspond au slug
+        return $this->render('marque/show.html.twig', [
+            'marque' => $marque,
+        ]);
     }
 
     #[Route('/modify/{slug}', requirements: ['slug' => '[a-z-]+'], name: "marque_modify")]
